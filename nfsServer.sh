2f331295ab67ce3c9c412cec6a4b8741c86c7100
Sakshi Kulkarni <67535376+Sakshi-Kulkarni@users.noreply.github.com>
2023-08-03 13:42:28 +0530

    Update nfsServer.sh

--- nfsServer.sh
+++ nfsServer.sh
@@ -2,6 +2,9 @@ sudo mkdir /test /test2
 sudo apt-get update
 sudo apt install nfs-kernel-server
 sudo vi /etc/exports 
+# Add directories to this file
+#   /test   *(rw,sync,no_root_squash,no_subtree_check)
+#   /test2  *(rw,sync,no_root_squash,no_subtree_check)
 sudo chmod 777 /test /test2
 sudo exportfs -r
 sudo systemctl restart nfs-kernel-server
